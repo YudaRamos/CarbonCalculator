1f343a9481de109577d6e04471133f9211509a13
Yudaisy Ramos Diaz <yudaisy.ramos.external@atos.net>
2022-05-29 11:12:56 +0200

    Creado proyecto base usando SpringBoot para la gestión del backend

---
+++ CarbonCalcutaorBackEnd/src/main/java/net/atos/zerokhoi/ZerokhoiApplication.java
@@ -0,0 +1,13 @@
+package net.atos.zerokhoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerokhoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerokhoiApplication.class, args);
+	}
+
+}
---
+++ CarbonCalcutaorBackEnd/src/test/java/net/atos/zerokhoi/ZerokhoiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.zerokhoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerokhoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
